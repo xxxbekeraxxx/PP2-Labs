46d691ea49ebff65772c1e489a82423d52426a6c
Yeraly28 <y_zharylkassyn@kbtu.kz>
2025-01-24 13:19:59 +0500

    Add files via upload

---
+++ lab2/Boolean.py
@@ -0,0 +1,19 @@
+#1
+print(10 > 9)
+#ans True
+
+#2 
+print(10 == 9)
+#ans False
+
+#3 
+print(10 < 9)
+#ans False
+
+#4
+print(bool("abc"))
+#ans True
+
+#5
+print(bool(0))
+#ans False
---
+++ lab2/forloops.py
@@ -0,0 +1,41 @@
+#1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+#2
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+#3
+for x in range(6):
+  print(x)
+
+#4
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+#5
+def my_function():
+  print("Hello from a function")
+
+#6
+def my_function():
+  print("Hello from a function")
+my_function()
+
+#7
+def my_function(fname, lname):
+  print(fname)
+
+#8
+def my_function(x):
+    return x+5
+
+
