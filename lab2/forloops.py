6f4ffd96f10768d66aa789c278d9d4391c36aff0
Yeraly28 <y_zharylkassyn@kbtu.kz>
2025-01-24 13:22:29 +0500

    Update forloops.py

--- lab2/forloops.py
+++ lab2/forloops.py
@@ -10,32 +10,5 @@ for x in fruits:
     continue
   print(x)
 
-#3
-for x in range(6):
-  print(x)
-
-#4
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  if x == "banana":
-    break
-  print(x)
-
-#5
-def my_function():
-  print("Hello from a function")
-
-#6
-def my_function():
-  print("Hello from a function")
-my_function()
-
-#7
-def my_function(fname, lname):
-  print(fname)
-
-#8
-def my_function(x):
-    return x+5
 
 
