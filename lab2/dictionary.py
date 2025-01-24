8e17dfab79135dcd29af20e7e6a9836266aa26f9
Yeraly28 <y_zharylkassyn@kbtu.kz>
2025-01-24 13:22:01 +0500

    Update dictionary.py

--- lab2/dictionary.py
+++ lab2/dictionary.py
@@ -1,4 +1,3 @@
-#1
 car =	{
   "brand": "Ford",
   "model": "Mustang",
@@ -6,34 +5,3 @@ car =	{
 }
 print(car.get("model"))
 
-#2
-car =	{
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-car["year"] = 2020
-
-#3
-car =	{
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-car["color"]="red"
-
-#4
-car =	{
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-car.pop("model")
-
-#5
-car =	{
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-car.clear()
