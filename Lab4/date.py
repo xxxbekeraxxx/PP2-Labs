7789f773c83c4933054eea47aaed2d45dfd76bf7
Yeraly <y_zharylkassyn@kbtu.kz>
2025-02-09 20:16:34 +0500

    hfkuyfi

--- Lab4/date.py
+++ Lab4/date.py
@@ -3,3 +3,12 @@ current_date = datetime.now()
 five_days_ago = current_date - timedelta(days=5)
 print("Current Date: ", current_date)
 print("Five days: ", five_days_ago)
+
+from datetime import datetime, timedelta
+current_date = datetime.now()
+yesterday = current_date - timedelta(days=1)
+today = current_date 
+tommorrow = current_date + timedelta(days=1)
+print("Yerterday: ", yesterday)
+print("Today: ", today)
+print("Tommorrow: ", tommorrow)
