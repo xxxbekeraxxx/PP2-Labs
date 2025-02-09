36e84caccfb7ab7f2e08bf06ecec805864b13de5
Yeraly <y_zharylkassyn@kbtu.kz>
2025-02-09 23:51:07 +0500

    dfvd

--- Lab4/date.py
+++ Lab4/date.py
@@ -4,6 +4,8 @@ five_days_ago = current_date - timedelta(days=5)
 print("Current Date: ", current_date)
 print("Five days: ", five_days_ago)
 
+print(" ")
+
 from datetime import datetime, timedelta
 current_date = datetime.now()
 yesterday = current_date - timedelta(days=1)
@@ -12,3 +14,22 @@ tommorrow = current_date + timedelta(days=1)
 print("Yerterday: ", yesterday)
 print("Today: ", today)
 print("Tommorrow: ", tommorrow)
+
+print(" ")
+
+from date import datetime
+current_datetime = datetime.now()
+datetime_without_microseconds = current_datetime.replace(microsecond=0)
+print("Datetime with microsecond: ", current_datetime)
+print("Datetime without microsecond: ", datetime_without_microseconds)
+
+print(" ")
+
+from datetime import datetime
+date1 = datetime(2025, 2, 9, 12, 0, 0 )
+date2 = datetime(2025, 2, 10, 12, 0 , 0)
+time_dif = date2 - date1
+seconds_differeence = time_dif.total_seconds()
+print("The difference is: ", seconds_differeence)
+
+
