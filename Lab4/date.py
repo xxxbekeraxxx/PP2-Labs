58bbb63e2de3ab9f4c298c3c32c498fedb428d35
Yeraly <y_zharylkassyn@kbtu.kz>
2025-02-09 19:49:31 +0500

    sdf

--- Lab4/date.py
+++ Lab4/date.py
@@ -1,1 +1,5 @@
-print("Hello World")
+from datetime import datetime, timedelta
+current_date = datetime.now()
+five_days_ago = current_date - timedelta(days=5)
+print("Current Date: ", current_date)
+print("Five days: ", five_days_ago)
